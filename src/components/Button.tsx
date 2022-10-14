fc8c7c140f568338aa8249aa8fd48944d2ab91dc
Matheus <mloubake@hotmail.com>
2022-10-13 21:49:07 -0300

    Creating Login Page with Design System Components.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,28 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Component = asChild ? Slot : "button";
 
   return (
     <Component
       className={clsx(
-        "px-3 py-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
-        {}
+        "px-4 py-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+        className
       )}
+      {...props}
     >
       {children}
     </Component>
